56db86e190b10b9b206dfdd9f6cea8117c71c38d
s-jez <stanislawjez7@gmail.com>
2022-07-03 16:22:04 +0200

    Add dropdown menu and fill from API

--- dogs.js
+++ dogs.js
@@ -1,34 +1,48 @@
 import { readDogs } from "./fetch_dogs.js";
 
-const BTN_SHOW = document.querySelector(".btn__show");
-const UL_LIST = document.querySelector(".dogs__list");
+const BTN_SHOW = document.querySelector(".btn-show");
+const DROPDOWN_MENU = document.querySelector(".dropdown-menu");
+const IMAGE_BREED = document.querySelector(".image-breed");
 
-let dogImage = [];
+let dogInfo = [],
+  dogImage = [];
+let selectedBreedId;
+
+const randomArray = (length, max) =>
+  [...new Array(length)].map(() => Math.round(Math.random() * max));
 
 const ShowBreeds = () => {
   const apiURL = "https://api.thedogapi.com/v1/breeds";
-  UL_LIST.textContent = "";
-  let items = Array.from(
-    { length: 40 },
-    (_, i) => i + Math.floor(Math.random() * 3)
-  );
+  DROPDOWN_MENU.textContent = "";
+  let items = randomArray(5, 100);
   readDogs(apiURL)
     .then((data) => {
-      dogImage = data;
+      dogInfo = data;
     })
     .then(() => {
-      items.forEach(function (id) {
-        const li = document.createElement("li");
-        const br = document.createElement("br");
-        if (dogImage !== "undefined") {
-          const dogName = document.createTextNode(dogImage[id].name);
-          li.appendChild(dogName);
-          UL_LIST.appendChild(br);
-          UL_LIST.appendChild(li);
+      items.forEach((id) => {
+        const btn = document.createElement("button");
+        btn.classList.toggle("dropdown-item");
+        btn.classList.toggle(`${id}`);
+        if (dogInfo !== "undefined") {
+          const dogName = document.createTextNode(dogInfo[id].name);
+          btn.appendChild(dogName);
+          DROPDOWN_MENU.appendChild(btn);
         }
+        btn.addEventListener("click", () => {
+          selectedBreedId = id;
+          if (selectedBreedId !== undefined) {
+            readDogs(apiURL).then((data) => {
+              dogImage = data;
+              IMAGE_BREED.src = data[selectedBreedId].image.url;
+            });
+          }
+        });
       });
-    });
+    })
+    .catch((err) => console.log(err));
 };
+
 BTN_SHOW.addEventListener("click", () => {
   ShowBreeds();
 });
